e66b16b040ed00c9accc38e32a5e7d25086b2d98
lyr0430 <786593295@qq.com>
2021-01-14 16:13:33 +0800

    完成了登录功能的开发

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="info" @click="signOut">退出</el-button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    signOut: function() {
+      window.sessionStorage.clear()
+      console.log(this.$router)
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped></style>
